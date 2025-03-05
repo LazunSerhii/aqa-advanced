686c625f8895bdd868d6d709db4c4bcbd9309752
Ваше Ім'я <lazunsm@gmail.com>
2025-03-05 23:33:58 +0200

    Додано файл з таблицею множення

---
+++ mywork/task5.4.js
@@ -0,0 +1,8 @@
+const number = 5;
+
+console.log(`Таблиця множення для числа ${number} (за допомогою циклу while):`);
+let i = 1;
+while (i <= 10) {
+    console.log(`${number} * ${i} = ${number * i}`);
+    i++;
+}
