69871c32e4e8750f68d09c05b9ac746d13b9f0f3
Ваше Ім'я <lazunsm@gmail.com>
2025-03-19 12:34:17 +0200

    Домашка9

---
+++ HomeWork9/task2.js
@@ -0,0 +1,12 @@
+const book = {
+    title: "Янголи і Демони",
+    author: "Ден Браун",
+    year: 2006
+  };
+  
+  
+  const { title, author } = book;
+  
+
+  console.log(title);  
+  console.log(author); 
