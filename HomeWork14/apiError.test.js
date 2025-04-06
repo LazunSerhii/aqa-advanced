f67f44dbe40d4098edc16a21ae0d4859c12a4adf
LazunSerhii <145491089+LazunSerhii@users.noreply.github.com>
2025-04-06 21:06:50 +0300

    Update apiError.test.js

--- HomeWork14/apiError.test.js
+++ HomeWork14/apiError.test.js
@@ -1,5 +1,36 @@
-const makeRequestToWrongUrl = require('./errorExample');
+const makeFailingRequest = require('./errorExample');
+const axios = require('axios');
 
-test('Простий тест', () => {
-  expect(1 + 1).toBe(2);
+jest.mock('axios');
+
+describe('Тестування обробки помилок Axios', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('має обробляти DNS помилки', async () => {
+    axios.get.mockRejectedValue({ code: 'ENOTFOUND' });
+
+    await expect(makeFailingRequest())
+      .rejects
+      .toThrow('DNS запит не вдалось виконати: сервер не знайдений');
+  });
+
+  it('має обробляти HTTP помилки', async () => {
+    axios.get.mockRejectedValue({ 
+      response: { status: 404 } 
+    });
+
+    await expect(makeFailingRequest())
+      .rejects
+      .toThrow('Сервер повернув помилку: 404');
+  });
+
+  it('має обробляти інші помилки', async () => {
+    axios.get.mockRejectedValue(new Error('Timeout exceeded'));
+
+    await expect(makeFailingRequest())
+      .rejects
+      .toThrow('Помилка при виконанні запиту: Timeout exceeded');
+  });
 });
